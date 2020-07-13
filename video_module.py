7306f8095b56fbaba265b2a5de972c889fd4d2bb
Ryu In Seong <insung1217@gmail.com>
2020-07-14 03:09:57 +0900

    feat: add face recognition test

--- video_module.py
+++ video_module.py
@@ -5,6 +5,9 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from track_gaze import get_gaze_ratio
 
+import numpy as np
+import face_recognition as fr
+
 class ImageViewer(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(ImageViewer, self).__init__(parent)
@@ -55,8 +58,38 @@ class ShowVideo(QtCore.QObject):
         # set predictor
         predictor = dlib.shape_predictor("shape_68.dat")
 
+        my_image = fr.load_image_file("inseong.jpeg")
+        my_face_encoding = fr.face_encodings(my_image)[0]
+
+        known_face_encodings = [my_face_encoding]
+        known_face_names = ["Inseong"]
+
         while self.run_video:
             ret, image = self.camera.read()
+            # face finder
+            rgb_frame = image[:, :, ::-1]
+            face_locations = fr.face_locations(rgb_frame)
+            face_encodings = fr.face_encodings(rgb_frame, face_locations)
+            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+                matches = fr.compare_faces(known_face_encodings, face_encoding)
+
+                name = "Unknown"
+
+                face_distances = fr.face_distance(known_face_encodings, face_encoding)
+
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            cv2.imshow('Webcam_facerecognition', image)
+            # end of face finder
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             # face detector
