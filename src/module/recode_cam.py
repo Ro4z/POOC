8e5f08f90edabcc903c80e2d60a7bbcea8d300b1
Ryu In Seong <insung1217@gmail.com>
2020-07-18 01:40:16 +0900

    feat: save recoding file in 'recode' directory

--- src/module/recode_cam.py
+++ src/module/recode_cam.py
@@ -6,7 +6,7 @@ class RecodeCam:
     today = date.today()
     file_name = today.strftime("%Y-%m-%d")
     file_name += '.avi'
-    out = cv2.VideoWriter(file_name, fourcc, 15.0, (640, 480))
+    out = cv2.VideoWriter("recode/" + file_name, fourcc, 15.0, (640, 480))
 
     def recode_cam(self, frame):
         self.out.write(frame)
